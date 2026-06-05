babcea41fd75afbe4d33b5ade7977e59a14e4b5a
Sergio Gastulo <sgastulo04@gmail.com>
2026-06-04 20:07:43 -0500

    added FDM for 1D heat equation, will work on animation

---
+++ py_v/utils/linalg.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy import sparse
+
+
+def tridiag(
+        n: int, 
+        diag: float, 
+        band: float, 
+        **kwargs
+) -> sparse.dia_matrix:
+    band_arr = np.full(n-1, band)
+    diag_arr = np.full(n, diag)
+    offset = [-1, 0, 1]
+    data = [band_arr, diag_arr, band_arr]
+    res = sparse.diags(data, offset, **kwargs)
+    return res
--- py_v/utils/visualization.py
+++ py_v/utils/visualization.py
@@ -30,23 +30,27 @@ def plot_diff_matrices(
         raise ValueError(
             f"Different shapes {mat1.shape} and {mat2.shape}: can't substract.")
 
-    diff = mat1 - mat2
-    mabs = np.abs(diff)
+    mabs = np.abs(mat1 - mat2)
+    p5, p95 = np.percentile(mabs, [5, 95])
+
     fig, ax = plt.subplots()
     im = ax.imshow(
         mabs, 
         cmap=plt.get_cmap('hot'), 
         aspect='auto',
-        **kwargs
+        vmin=p5, 
+        vmax=p95,
+        **kwargs,
     )
     fig.colorbar(im)
 
-    title = f'Residual |max|={np.max(mabs):.2e} |mean|={np.mean(mabs):.2e}'
-    ax.set_title(
-        title,
-        fontsize=10, 
-        pad=12
+    title = (
+        f" Statistics:"
+        f"\nmax = {np.max(mabs):.2e}"
+        f"\nmean = {np.mean(mabs):.2e}"
+        f"\np95 = {p95:.2e}"
     )
-
-    fig.tight_layout()
+    fig.supxlabel(title, x=0.5, fontsize=10, ha='center')
+    fig.suptitle("Plot: np.abs(mat1 - mat2)")
+    fig.tight_layout(rect=[0, 0.1, 1, 0.95])
     return fig
